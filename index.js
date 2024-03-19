4f8111f694e4e24bd7aef0a1d90d11d5aa2ca883
Benhin <benhinmwendwa7@gmail.com>
2024-03-19 14:49:42 +0300

    condition

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Value){
   // Write your code here!
+if(Value <= 400){
+  return "This one is on me!"
+} else if (Value > 399 &&  Value < 2000){
+  return `That will be twenty bucks.`
+}
+ else if (2000<Value && Value<=2500){
+  return `I will gladly take your thirty bucks.`
+ }
+ else 
+  return "No can do." 
+ 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(City){
   // Write your code here!
+  if (City==`NYC`){
+    return "Ok, sounds good."
+  }
+  else 
+  return "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
-}
+  switch (Tip) {
+    case 'generous':
+        return 'Thank you so much.';
+    case 'not as generous':
+        return 'Thank you.';
+    default:
+        return 'Bye.';
+  
+}}
